b80b0255f12162769ed4d5a3932bb277d304d985
coolkid23319 <162951844+coolkid23319@users.noreply.github.com>
2024-04-14 23:49:35 -0400

    Update bladify.lua

--- bladify.lua
+++ bladify.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://paste.gg/p/anonymous/a3df67bcf191405e8004bf4498776eae/files/f343488e55d04b2fb612cac6504f9d3c/raw'))()
+loadstring(game:HttpGet('https://paste.gg/p/anonymous/63d7db999a0a4081a147a0631aae09e1/files/842ddf5ab4cd4d7baa3b96fb8ffcd741/raw'))()
